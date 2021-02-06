ba14449792104aae0ad44d129335cda9152a825d
Jai-Coders-Gang <76532161+Jai-Coders-Gang@users.noreply.github.com>
2021-02-06 13:19:49 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,33 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+var box1, box2, box3, box4, box5;
+var ground;
+
+function setup(){
+    var canvas = createCanvas(400,400);
+    engine = Engine.create();
+    world = engine.world;
+
+    
+    ground = new Ground(200,390,400,20);
+    box1 = new Box(250,300,50,50);
+    box2 = new Box(200,100,50,100);
+    box3 = new Box(100,300,50,50);
+    box4 = new Box(150,100,50,100);
+    box5 = new Box(180,350,50,70);}
+    
+
+function draw(){
+    background(0);
+    Engine.update(engine);
+    box1.display();
+    box2.display();
+    box3.display();
+    box4.display();
+    box5.display();
+    ground.display();
+   
+}
